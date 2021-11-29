6132e7ab3572b2adc76fe42b5a2b77cd1e97459a
roby-dev <45070139+roby-dev@users.noreply.github.com>
2021-11-29 10:35:06 -0500

    ra

--- backendSGCS/Controllers/ReportController.cs
+++ backendSGCS/Controllers/ReportController.cs
@@ -33,6 +33,7 @@ namespace backendSGCS.Controllers {
                 Result.cantidadSolicitud = getSolicitudByProjectsByUser(_usuario.IdUsuario).Count;
                 Result.cantidadMiembroProyecto = getMembersByUser(_usuario.IdUsuario).Count;
                 if (_usuario.Rol == "USER") {
+                    Result.cantidadSolicitud = getSolicitudByProjectsByUser(_usuario.IdUsuario).Where(x=>x.IdMiembroProyectoNavigation.IdUsuario==_usuario.IdUsuario).ToList().Count;
                     Result.cantidadCargo = 0;
                     Result.cantidadEntregable = 0;
                     Result.cantidadElementoConfiguracion = 0;
